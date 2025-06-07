0488a276ab84a9f861bd0c75a5561dd3fbae0150
unknown <venthon71@gmail.com>
2025-06-07 15:50:57 +0700

    to do ui pagination

--- components/TopicList.jsx
+++ components/TopicList.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import RemoveBtn from './RemoveBtn';
 import Link from 'next/link';
 import {HiPencilAlt} from "react-icons/hi"
+import { Pagination, PaginationContent, PaginationEllipsis, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from './ui/pagination';
 
 const getTopicsApi = async() => {
   try {
@@ -39,6 +40,28 @@ export default async function TopicList() {
         </div>
       </div>
     ))}
+    <Pagination>
+      <PaginationContent>
+        <PaginationItem>
+          <PaginationPrevious href="#" />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#"> 1</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#" isActive>2</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">3</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationEllipsis />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationNext href="#" />
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
     </>
   );
 }
